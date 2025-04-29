f71090e1a649168341ccaee13b70370a597a316b
subrattech <beherasubrat5040@gmail.com>
2025-04-29 16:06:28 +0530

    xc

--- src/main/java/com/coolcoder/controller/AuthController.java
+++ src/main/java/com/coolcoder/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import com.coolcoder.service.UserService;
 
 @RestController
 @RequestMapping("/auth")
+//@CrossOrigin(origins = "http://127.0.0.1:5500/")
 public class AuthController {
 
     @Autowired private UserService userService;
